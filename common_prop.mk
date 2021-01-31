18fe437a82a7a566154c276b3a40c0812032636a
kdrag0n <danny@kdrag0n.dev>
2021-02-01 04:12:25 +0800

    sdm660-common: Switch Skia rendering backend to Vulkan This results in slightly lower frame times in most UiBench tests:
    Skia + Adreno OpenGL:
                                Score   Jank Frames Bad Frames  Avg Frame Time (ms)
    Overdraw Test               1       0.01%       0.06%       3.16
    Edit Text Input             5       0.13%       4.99%       7.79
    High-hitrate text render    1       0.01%       0.51%       6.81
    Image List View Fling       1       0.01%       0.32%       6.73
    Bitmap Upload Test          0       0.01%       0.99%       9.38
    Low-hitrate text render     1       0.01%       0.49%       7.08
    List View Fling             23      0.12%       0.78%       6.61
    Shadow Grid Fling           16      0.10%       0.94%       7.32
    
    Skia + Adreno Vulkan:
                                Score   Jank Frames Bad Frames  Avg Frame Time (ms)
    Overdraw Test               1       0.01%       0.06%       2.94
    Edit Text Input             6       0.13%       4.60%       8.00
    High-hitrate text render    1       0.01%       0.28%       6.29
    Image List View Fling       1       0.01%       0.36%       5.96
    Bitmap Upload Test          1       0.01%       0.15%       9.25
    Low-hitrate text render     1       0.01%       0.33%       6.71
    List View Fling             16      0.09%       2.70%       6.62
    Shadow Grid Fling           11      0.07%       0.49%       6.98
    
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- common_prop.mk
+++ common_prop.mk
@@ -154,6 +154,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
+    debug.hwui.renderer=skiavk \
     debug.sf.latch_unsignaled=1 \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.hw=1 \
