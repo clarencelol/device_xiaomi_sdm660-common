3dce3fec61da926bbb0be4f18712df39e43b622d
Jabiyeff <cebiyevanar@gmail.com>
2021-03-01 03:40:05 +0800

    sdm660-common: build new sensor blobs
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- sdm660.mk
+++ sdm660.mk
@@ -501,6 +501,8 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service \
+    android.hardware.sensors@2.0 \
+    android.hardware.sensors@2.1 \
     libsensorndkbridge
 
 # Sensor Config
