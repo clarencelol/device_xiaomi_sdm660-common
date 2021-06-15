51935df91805e81e58cf96cd816bcc92c1a210c9
clarencelol <clarencekuiek@icloud.com>
2021-06-15 21:45:26 +0600

    sdm660-common: Switch to Thermal 2.0 mock * QTI Thermal couldn't read the temperature for some reason
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- sdm660.mk
+++ sdm660.mk
@@ -550,7 +550,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti
+    android.hardware.thermal@2.0-service.mock
 
 # Thermal Conf
 PRODUCT_COPY_FILES += \
