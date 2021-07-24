0c553b59b7786f9688ced5b1bbcd12d8a7dca52f
clarencelol <clarencekuiek@icloud.com>
2021-07-24 12:31:52 +0800

    sdm660-common: ShapeShiftOS Bringup

--- sdm660.mk
+++ sdm660.mk
@@ -387,7 +387,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(COMMON_PATH)/overlay \
-    $(COMMON_PATH)/overlay-awaken \
+    $(COMMON_PATH)/overlay-ssos \
     $(COMMON_PATH)/overlay-lineage
 
 # RRO configuration
