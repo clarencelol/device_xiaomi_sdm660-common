eefb63f2e9241d8d531833116b0c7e30c4990155
Marc Bourgoin <themard85@gmail.com>
2021-10-17 00:21:31 +0800

    sdm660-common: Drop QCOM WFD
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- sdm660.mk
+++ sdm660.mk
@@ -653,15 +653,6 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
     $(COMMON_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
-# WiFi Display
-PRODUCT_PACKAGES += \
-    libaacwrapper \
-    libnl
-
-# WiFi Display JAR
-#PRODUCT_BOOT_JARS += \
-#    WfdCommon
-   
 # IPA
 USE_DEVICE_SPECIFIC_DATA_IPA_CFG_MGR := true
 USE_DEVICE_SPECIFIC_IPACFG_MGR := true
