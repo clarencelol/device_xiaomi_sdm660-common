b004640ca6ce33eb0e39d2267c6450e93b1cbd09
clarencelol <clarencekuiek@icloud.com>
2021-10-18 02:26:54 +0800

    sdm660-common: Switch back to mock powerstats
    [Squashed revert commits]
    This reverts commit https://github.com/clarencelol/android_device_xiaomi_sdm660-common/commit/57e9c72e3ccfec01a345c3d19bfc96773c472be6
    This reverts commit https://github.com/clarencelol/android_device_xiaomi_sdm660-common/commit/d46a1c658c9492b9478b0d69a2470e47f3738539
    This reverts commit https://github.com/clarencelol/android_device_xiaomi_sdm660-common/commit/c5294b5e285396885209b7680f83b441b5b05fce
    This reverts commit https://github.com/clarencelol/android_device_xiaomi_sdm660-common/commit/28af98a7a6102b24e8d335997a17b6d3530f36ae
    This reverts commit https://github.com/clarencelol/android_device_xiaomi_sdm660-common/commit/44988b254bcdf7e7a010ad2de3dbfe68e76b181b
    
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- sdm660.mk
+++ sdm660.mk
@@ -480,7 +480,7 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi_sdm660-libperfmgr \
-    android.hardware.power.stats@1.0-service.xiaomi_sdm660
+    android.hardware.power.stats@1.0-service.mock
 
 # Powerhint
 ifeq ($(EAS_POWERHINT_VARIANT), sdm636)
