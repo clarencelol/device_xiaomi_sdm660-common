fd50ffdce148a92bbd6997a0fb7fbb93b70d9886
clarencelol <clarencekuiek@icloud.com>
2022-01-14 14:43:54 +0800

    sdm660-common: overlay: Evolution-ify
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- sdm660.mk
+++ sdm660.mk
@@ -383,7 +383,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(COMMON_PATH)/overlay \
-    $(COMMON_PATH)/overlay-ssos \
+    $(COMMON_PATH)/overlay-evolution \
     $(COMMON_PATH)/overlay-lineage
 
 # RRO configuration
